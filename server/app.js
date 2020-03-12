818eb4aa56646d782c5d8dfc34a4e8462acb133c
AndrewAkram97 <51288273+AndrewAkram97@users.noreply.github.com>
2020-03-12 15:38:10 +0200

    feat: add mongo db connection and models

--- server/app.js
+++ server/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const mongoose = require('mongoose');
 
 const schema = require('./schema/schema');
-
 const app = express();
+
+mongoose.connect('mongodb+srv://andrew:Andrew123@graphql-cluster-bizau.mongodb.net/test?retryWrites=true&w=majority',
+ { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connection.once('open', () => {
+  console.log('Mongo db is connected');
+});
 
 app.use('/graphql', graphqlHTTP({
   graphiql: true,
